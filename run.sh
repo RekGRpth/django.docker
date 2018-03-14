61a55b03226bf947831d680f8a823f0f3c502cfc
RekGRpth <rekgrpth@gmail.com>
2018-03-14 09:14:05 +0500

    up

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-docker stop django && \
-docker rm django && \
+#docker stop django && \
+#docker rm django && \
 docker pull rekgrpth/django && \
 docker volume create django && \
 docker run \
