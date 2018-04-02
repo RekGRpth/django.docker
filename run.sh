4100aa97434035173f516b2052b1331e56e566de
RekGRpth <rekgrpth@gmail.com>
2018-04-02 07:58:49 +0500

    up

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-#docker build --tag rekgrpth/django . && \
-#docker push rekgrpth/django
+#docker build --tag rekgrpth/django . || exit $?
+#docker push rekgrpth/django || exit $?
 docker stop django
 docker rm django
-docker pull rekgrpth/django && \
-docker volume create django && \
+docker pull rekgrpth/django || exit $?
+docker volume create django || exit $?
 docker run \
     --add-host `hostname -f`:`ip -4 addr show docker0 | grep -oP 'inet \K[\d.]+'` \
     --add-host ldap.t72.ru:`getent hosts ldap.t72.ru | cut -d ' ' -f 1` \
